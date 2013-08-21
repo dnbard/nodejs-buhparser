18fdc3e297c6e48450bb11248b231fb2b178c442
Alex Bardanov <dnbard@gmail.com>
2013-08-21 15:55:49 +0300

    start server after db connection opened

--- database.js
+++ database.js
@@ -29,28 +29,32 @@ var generate_mongo_url = function(obj){
     return "mongodb://first:jqueen@paulo.mongohq.com:10087/buh-db";
 }
 
-var reconnTimer = null;
+exports.Connect = Connect;
+function Connect(callback){
+    var reconnTimer = null;
 
-var mongourl = generate_mongo_url(mongo);
-mongoose.connect(mongourl);
-var db = mongoose.connection;
-db.on('error', function(){
-	cc.log(cc.error("Can't connect to " + mongourl));    
-});
-db.on('close', function() {
-    cc.log(cc.error("Connection to " + mongourl + " is closed"));
-    cc.log("Trying reconnect to " + mongourl);
-    if (reconnTimer) { }
-    else {
-    reconnTimer = setTimeout(tryReconnect, 500); // try after delay
-    }
-});
-db.on('open', function() {
-	cc.log(cc.ok('Connected to ' + cc.bold(mongourl)));
-    if (reconnTimer) { clearTimeout(reconnTimer); reconnTimer = null; }
-});
-
-function tryReconnect() {
-  reconnTimer = null;  
-  db = mongoose.connect(generate_mongo_url(mongo));
+    var mongourl = generate_mongo_url(mongo);
+    mongoose.connect(mongourl);
+    var db = mongoose.connection;
+    db.on('error', function(){
+        cc.log(cc.error("Can't connect to " + mongourl));    
+    });
+    db.on('close', function() {
+        cc.log(cc.error("Connection to " + mongourl + " is closed"));
+        cc.log("Trying reconnect to " + mongourl);
+        if (reconnTimer) { }
+        else {
+        reconnTimer = setTimeout(tryReconnect, 500); // try after delay
+        }
+    });
+    db.on('open', function() {
+        cc.log(cc.ok('Connected to ' + cc.bold(mongourl)));
+        if (reconnTimer) { clearTimeout(reconnTimer); reconnTimer = null; }
+        return callback();
+    });
+    
+    function tryReconnect() {
+        reconnTimer = null;  
+        db = mongoose.connect(generate_mongo_url(mongo));
+    };
 }
--- server.js
+++ server.js
@@ -4,20 +4,22 @@ var express = require('express'),
 	parser = require('./parser/command.js'),
 	routing = require('./routing.js'),
 	bootstrap = require('./bootstrap.js')
-	timers = require('./timers.js');
-
-var app = express();
-
-//var port = process.env.VCAP_APP_PORT || 3000;
-var port = process.env.PORT || 3000;
-app.listen(port);
+	timers = require('./timers.js'), 
+	db = require('./database.js');
 
 cc.log('<=================================================>');
-cc.log(cc.ok('Server started at ' + cc.bold(port) + ' port'));
+var app = express();
+db.Connect(function(){
+	//var port = process.env.VCAP_APP_PORT || 3000;
+	var port = process.env.PORT || 3000;
+	app.listen(port);
 
-app.use(bootstrap.beforeRequest);
-app.use(bootstrap.errorLogging);
-routing.init(app);
+	cc.log(cc.ok('Server started at ' + cc.bold(port) + ' port'));
 
-timers.startup();
-timers.init(1500000);
+	app.use(bootstrap.beforeRequest);
+	app.use(bootstrap.errorLogging);
+	routing.init(app);
+
+	timers.startup();
+	timers.init(1500000);
+});
